3b24aae827dc01dcaf7183f6a682d873e718995b
Konstaninos Kousounnis <39504405+kkousounnis@users.noreply.github.com>
2020-10-13 22:33:27 +0300

    Add files via upload

---
+++ taco-cloud/src/test/java/com/mygroup/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,14 @@
+package com.mygroup.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	
+
+}
